9a737866ea72482ab5dd2a8395f576fc789d1536
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-05-14 16:44:42 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # psmove_dk2_coreg
 A simple tool to coregister the PSMove and DK2
+
+# Warnings
+
+1. There is no error checking.
+2. Mac support does not work yet.
+3. This is not intended for general use. It is intended only for a few specific programmers. I do not intend to make this user-friendly or to support it beyond getting basic functionality down.
+
+# Instructions
+
+1. Clone the repo.
+2. mkdir build
+3. cd build
+4. cmake-gui ..
+    * Only VS2013 supported right now.
+5. Build
+    * Open the VS2013 project
+    * Change the target to Release W64
+    * Build solution
+6. Copy the DLLs from 3rdparty/psmoveapi/lib/win64 to wherever your .exe was made (probably build/Release)
+7. Make sure your PSMove and DK2 are both in view of their cameras.
+8. Run psmdk2.exe
+    * Alternatively, if you want to output the data to a file, try `psmdk2 > output.csv`
+9. Press & Hold the PSMove Move button to print data (to screen or file)
+10. Press PSMove select button to quit.
---
+++ src/psmdk2.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <assert.h>
+#include <conio.h>
+#include "psmove.h"
+#include "psmove_tracker.h"
+#include "OVR_CAPI.h"
+#include "OVR_CAPI_Keys.h"
+
+int main(int arg, char** args) {
+
+    // Setup PSMove
+    int count = psmove_count_connected();
+    PSMove **controllers = (PSMove **)calloc(count, sizeof(PSMove *));
+    PSMoveTracker* tracker = psmove_tracker_new();
+    psmove_tracker_set_mirror(tracker, PSMove_True);
+    
+    int result;
+    int i = 0;
+    controllers[i] = psmove_connect_by_id(i);
+    while (1) {
+        result = psmove_tracker_enable(tracker, controllers[i]);
+        if (result == Tracker_CALIBRATED) {
+            enum PSMove_Bool auto_update_leds =
+                psmove_tracker_get_auto_update_leds(tracker,
+                        controllers[i]);
+            break;
+        } else {
+            printf("ERROR - retrying\n");
+        }
+    }
+    assert(psmove_has_calibration(controllers[i]));
+    psmove_enable_orientation(controllers[i], PSMove_True);
+    assert(psmove_has_orientation(controllers[i]));
+    int buttons = psmove_get_buttons(controllers[i]);
+    
+    // TODO: Setup DK2
+    ovrBool ovrresult;
+    ovrresult = ovr_Initialize(0);
+    ovrHmd HMD = ovrHmd_Create(0);
+    ovrresult = ovrHmd_ConfigureTracking(HMD,
+                ovrTrackingCap_Orientation |
+                ovrTrackingCap_MagYawCorrection |
+                ovrTrackingCap_Position, 0);
+    ovrTrackingState dk2state;
+    
+    int ch;
+    printf("psm_px,psm_py,psm_pz,psm_ox,psm_oy,psm_oz,psm_ow,dk2_px,dk2_py,dk2_pz,dk2_ox,dk2_oy,dk2_oz,dk2_ow\n");
+    while (1)//(ch = _getch()) != EOF && ch != 'q')
+    {
+        psmove_tracker_update_image(tracker);
+        psmove_tracker_update(tracker, NULL);
+
+        // Get PSMove position
+        float psm_px, psm_py, psm_pz;
+        psmove_tracker_get_location(tracker, controllers[i], &psm_px, &psm_py, &psm_pz);
+        
+        // Get PSMove orientation
+        while (psmove_poll(controllers[i]));
+        float psm_ox, psm_oy, psm_oz, psm_ow;
+        psmove_get_orientation(controllers[i], &psm_ow, &psm_ox, &psm_oy, &psm_oz);
+
+        // Update DK2 state
+        dk2state = ovrHmd_GetTrackingState(HMD, 0.0);
+        
+        buttons = psmove_get_buttons(controllers[i]);
+
+        if (buttons & Btn_MOVE)
+            printf("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+                psm_px, psm_py, psm_pz,
+                psm_ox, psm_oy, psm_oz, psm_ow,
+                dk2state.HeadPose.ThePose.Position.x,
+                dk2state.HeadPose.ThePose.Position.y,
+                dk2state.HeadPose.ThePose.Position.z,
+                dk2state.HeadPose.ThePose.Orientation.x,
+                dk2state.HeadPose.ThePose.Orientation.y,
+                dk2state.HeadPose.ThePose.Orientation.z,
+                dk2state.HeadPose.ThePose.Orientation.w);
+
+        if (buttons & Btn_SELECT)
+            break;
+    }
+    
+    psmove_disconnect(controllers[i]);
+    psmove_tracker_free(tracker);
+    free(controllers);
+    
+    ovrHmd_Destroy(HMD);
+    ovr_Shutdown();
+    
+    return 0;
+}
