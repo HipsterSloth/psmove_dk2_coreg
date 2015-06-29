fe0c619fba4d210c6ee56342471918ce2a96463a
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-06-29 18:14:32 -0400

    Matlab test now calculates transform to camera space, then tests transform after transforming to camera space then hmd space.

--- matlab/test.m
+++ matlab/test.m
@@ -1,7 +1,14 @@
 %% Load the data
 
 poses = readtable(fullfile(pwd,'output.txt'));
+% poses.dk2_px = poses.dk2_px / 100;
+% poses.dk2_py = poses.dk2_py / 100;
+% poses.dk2_pz = poses.dk2_pz / 100;
 camera_pose = csvread(fullfile(pwd,'output_camerapose.txt'));
+% camera_pose(1:3) = camera_pose(1:3)/100;
+camera_4x4 = makehgtform('translate', camera_pose(1:3),...
+    'axisrotate', camera_pose(4:6), camera_pose(7));
+camera_invxform = [camera_4x4(1:3,1:3)', -camera_4x4(1:3,1:3)'*camera_4x4(1:3,4); 0 0 0 1];
 
 n = size(poses, 1);
 
@@ -12,10 +19,13 @@ for p = 1:n
     h = makehgtform(...
         'translate', [poses.dk2_px(p), poses.dk2_py(p), poses.dk2_pz(p)],...
         'axisrotate', [poses.dk2_ox(p), poses.dk2_oy(p), poses.dk2_oz(p)], poses.dk2_ow(p));
+    h = camera_invxform * h;
     RMi = h(1:3, 1:3)';
+    
     Ti = makehgtform(...
         'translate', [poses.psm_px(p), poses.psm_py(p), poses.psm_pz(p)],...
         'axisrotate', [poses.psm_ox(p), poses.psm_oy(p), poses.psm_oz(p)], poses.psm_ow(p));
+    
     A((p-1)*3 + (1:3), :) = [RMi * Ti(1,4), RMi * Ti(2,4), RMi * Ti(3,4), RMi, -eye(3)];
     b((p-1)*3 + (1:3)) = RMi * h(1:3, 4);
 end
@@ -29,8 +39,9 @@ clear A b p h RMi Ti x
 
 %% Plot the result
 dk2_xyz = [poses.dk2_px, poses.dk2_py, poses.dk2_pz]';
+%dk2_xyz = camera_invxform(1:3,:) * [dk2_xyz; ones(1, n)];
 psm_xyz = [poses.psm_px, poses.psm_py,  poses.psm_pz]';
-lims = max([abs(psm_xyz)'; abs(dk2_xyz)']);
+lims = [min([psm_xyz'; dk2_xyz']); max([psm_xyz'; dk2_xyz'])];
 subplot(2,2,1)
 plot3(dk2_xyz(1,:), dk2_xyz(3,:), dk2_xyz(2,:), 'k',...
     psm_xyz(1,:), psm_xyz(3,:), psm_xyz(2,:), 'm',...
@@ -38,14 +49,14 @@ plot3(dk2_xyz(1,:), dk2_xyz(3,:), dk2_xyz(2,:), 'k',...
 xlabel('X')
 ylabel('Z')
 zlabel('Y')
-xlim([-lims(1) lims(1)]);
-ylim([-lims(2) lims(2)]);
-zlim([-lims(3) lims(3)]);
+xlim([lims(1,1) lims(2,1)]);
+ylim([lims(1,3) lims(2,3)]);
+zlim([lims(1,2) lims(2,2)]);
 legend('DK2', 'PSMove', 'Location', 'North')
 legend('boxoff')
 set(gca, 'FontSize', 14)
 set(gca, 'Color', 'none')
-set(gca, 'CameraViewAngle', 8.7)
+%set(gca, 'CameraViewAngle', 8.7)
 
 subplot(2,2,2)
 hist(sqrt(sum((dk2_xyz - psm_xyz).^2)));
@@ -56,7 +67,8 @@ set(gca, 'FontSize', 14)
 title('Before Coreg.')
 box off
 
-tpsm_xyz = globalxfm * [psm_xyz; ones(1, n)];
+tpsm_xyz = camera_4x4 * [globalxfm; 0 0 0 1] * [psm_xyz; ones(1, n)];
+tpsm_xyz = tpsm_xyz(1:3, :);
 subplot(2,2,3)
 plot3(dk2_xyz(1,:), dk2_xyz(3,:), dk2_xyz(2,:), 'k',...
     tpsm_xyz(1,:), tpsm_xyz(3,:), tpsm_xyz(2,:), 'm',...
@@ -64,15 +76,16 @@ plot3(dk2_xyz(1,:), dk2_xyz(3,:), dk2_xyz(2,:), 'k',...
 xlabel('X')
 ylabel('Z')
 zlabel('Y')
-xlim([-lims(1) lims(1)]);
-ylim([-lims(2) lims(2)]);
-zlim([-lims(3) lims(3)]);
+lims = [min([tpsm_xyz'; dk2_xyz']); max([tpsm_xyz'; dk2_xyz'])];
+xlim([lims(1,1) lims(2,1)]);
+ylim([lims(1,3) lims(2,3)]);
+zlim([lims(1,2) lims(2,2)]);
 set(gca, 'FontSize', 14)
 set(gca, 'Color', 'none')
-set(gca, 'CameraViewAngle', 8.7)
+%set(gca, 'CameraViewAngle', 8.7)
 
 subplot(2,2,4)
-hist(sqrt(sum((dk2_xyz - tpsm_xyz).^2)));
+hist(sqrt(sum((dk2_xyz - tpsm_xyz(1:3,:)).^2)));
 xlabel('Eucl. Distance (cm)')
 ylabel('Count')
 title('After Coreg.')
